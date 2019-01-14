31c78e38ede85dd6a8692d55a48f6f89bcca8fa8
John Cido <johncido@outlook.com>
2019-01-14 15:45:28 +0800

    >Clean up npm scripts >Update small icons

--- webpack.config.js
+++ webpack.config.js
@@ -6,126 +6,128 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 const ChromeExtensionReloader = require('webpack-chrome-extension-reloader')
 
-const mode = process.env.NODE_ENV
-const isDevelopMode = mode === 'development'
+module.exports = env => {
+    const mode = env.NODE_ENV
+    const dev = mode === 'development'
 
-module.exports = {
-    mode,
-    devtool: isDevelopMode ? 'inline-source-map' : 'nosources-source-map',
-    entry: {
-        background: './src/js/main.ts',
-        favorite: './src/js/content-page/favorite.ts',
-        video: './src/js/content-page/video.ts',
-        popup: './src/js/popup.ts'
-    },
-    output: {
-        path: path.resolve(__dirname, `${mode}/`),
-        filename: '[name].js'
-    },
+    return {
+        mode,
+        devtool: dev ? 'inline-source-map' : 'nosources-source-map',
+        entry: {
+            background: './src/js/main.ts',
+            favorite: './src/js/content-page/favorite.ts',
+            video: './src/js/content-page/video.ts',
+            popup: './src/js/popup.ts'
+        },
+        output: {
+            path: path.resolve(__dirname, `${mode}/`),
+            filename: '[name].js'
+        },
 
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: 'babel-loader'
-            }, {
-                test: /\.ts$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'ts-loader',
-                    options: {
-                        appendTsSuffixTo: [/\.vue$/]
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: 'babel-loader'
+                }, {
+                    test: /\.ts$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'ts-loader',
+                        options: {
+                            appendTsSuffixTo: [/\.vue$/]
+                        }
                     }
+                }, {
+                    test: /\.less$/,
+                    use: ExtractTextPlugin.extract({
+                        use: [
+                            'css-loader',
+                            'less-loader'
+                        ],
+                        fallback: 'style-loader'
+                    })
+                }, {
+                    test: /\.scss$/,
+                    use: ExtractTextPlugin.extract({
+                        use: [
+                            'css-loader',
+                            {
+                                loader: 'sass-loader',
+                                options: {
+                                    sourceMap: false,
+                                    includePaths: [ './node_modules' ]
+                                }
+                            }
+                        ],
+                        fallback: 'style-loader'
+                    })
+                }, {
+                    test: /\.pug$/,
+                    exclude: /node_modules/,
+                    use: 'pug-plain-loader'
+                }, {
+                    test: /\.vue$/,
+                    exclude: /node_modules/,
+                    use: 'vue-loader'
+                }, {
+                    test: /\.(png|jpg|jpeg|woff|woff2|eot|ttf|svg)$/,
+                    use: 'url-loader'
                 }
-            }, {
-                test: /\.less$/,
-                use: ExtractTextPlugin.extract({
-                    use: [
-                        'css-loader',
-                        'less-loader'
-                    ],
-                    fallback: 'style-loader'
+            ]
+        },
+
+        resolve: {
+            extensions: ['.js', '.ts', '.es6', '.json', '.less', '.vue', '.scss']
+        },
+
+        plugins: [
+            new VueLoaderPlugin(),
+            new CopyFilesPlugin([
+                { from: 'src/manifest.json', to: './manifest.json' },
+                { from: 'src/html', to: './' },
+                { from: 'src/_locales', to: './_locales' },
+                { from: 'src/img/icons', to: './img/icons' }
+            ], {
+                ignore: [ '.*' ],
+                copyUnmodified: true,
+                debug: 'warning'
+            }),
+            new ExtractTextPlugin({ filename: '[name].css', disable: false, allChunks: true }),
+            ...(dev ? [
+                new ChromeExtensionReloader({
+                    entries: {
+                        contentScript: [ 'favorite', 'video' ]
+                    }
                 })
-            }, {
-                test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    use: [
-                        'css-loader',
-                        {
-                            loader: 'sass-loader',
-                            options: {
-                                sourceMap: false,
-                                includePaths: [ './node_modules' ]
+            ] : [
+                new webpack.DefinePlugin({
+                    'process.env': {
+                        NODE_ENV: JSON.stringify('production')
+                    }
+                })
+            ])
+        ],
+
+        optimization: {
+            splitChunks: {
+                name: 'common'
+            },
+            ...(dev ? {} : {
+                minimizer: [
+                    new TerserPlugin({
+                        cache: true,
+                        parallel: true,
+                        terserOptions: {
+                            ecma: 6,
+                            output: {
+                                ascii_only: true
                             }
                         }
-                    ],
-                    fallback: 'style-loader'
-                })
-            }, {
-                test: /\.pug$/,
-                exclude: /node_modules/,
-                use: 'pug-plain-loader'
-            }, {
-                test: /\.vue$/,
-                exclude: /node_modules/,
-                use: 'vue-loader'
-            }, {
-                test: /\.(png|jpg|jpeg|woff|woff2|eot|ttf|svg)$/,
-                use: 'url-loader'
-            }
-        ]
-    },
-
-    resolve: {
-        extensions: ['.js', '.ts', '.es6', '.json', '.less', '.vue', '.scss']
-    },
-
-    plugins: [
-        new VueLoaderPlugin(),
-        new CopyFilesPlugin([
-            { from: 'src/manifest.json', to: './manifest.json' },
-            { from: 'src/html', to: './' },
-            { from: 'src/_locales', to: './_locales' },
-            { from: 'src/img/icons', to: './img/icons' }
-        ], {
-            ignore: [ '.*' ],
-            copyUnmodified: true,
-            debug: 'warning'
-        }),
-        new ExtractTextPlugin({ filename: '[name].css', disable: false, allChunks: true }),
-        ...(isDevelopMode ? [
-            new ChromeExtensionReloader({
-                entries: {
-                    contentScript: [ 'favorite', 'video' ]
-                }
+                    })
+                ]
             })
-        ] : [
-            new webpack.DefinePlugin({
-                'process.env': {
-                    NODE_ENV: JSON.stringify('production')
-                }
-            })
-        ])
-    ],
-
-    optimization: {
-        splitChunks: {
-            name: 'common'
-        },
-        ...(isDevelopMode ? {} : {
-            minimizer: [
-                new TerserPlugin({
-                    cache: true,
-                    parallel: true,
-                    terserOptions: {
-                        ecma: 6,
-                        output: {
-                            ascii_only: true
-                        }
-                    }
-                })
-            ]
-        })
+        }
     }
 }
